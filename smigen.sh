b1c40680c05aa5cc380dc36a8cb4a8732059a740
yulyugin <yulyugin@gmail.com>
2016-03-24 17:24:12 +0300

    Implementa shell script to run smigen.

---
+++ smigen.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+SMIGEN_DRIVER_KO=build-smigen-`uname -r`/smigen.ko
+SMIGEN_EXE=smigen.exe
+
+[ -e $SMIGEN_DRIVER_KO ] || { echo "$SMIGEN_DRIVER_KO does not exist" ; exit 1 ; }
+[ -e $SMIGEN_EXE ] || { echo "$SMIGEN_EXE does not exist" ; exit 1 ; }
+
+SUDO=""
+if [ ! -z `id -u` ] ; then
+   SUDO="sudo"
+fi
+
+$SUDO /sbin/rmmod smigen
+$SUDO /sbin/insmod $SMIGEN_DRIVER_KO
+
+RET=$?
+if [ $RET -ne 0 ] ; then
+    echo "Failed to load $SMIGEN_DRIVER_KO"
+    exit $RET
+fi
+
+$SUDO `readlink -f $SMIGEN_EXE`
+
